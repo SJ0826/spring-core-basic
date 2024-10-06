91145451cff52f6cc74be37ff9e82ea663977a98
SJ0826 <ikosdu60@gmail.com>
2024-10-06 16:24:28 +0900

    [Section7] 중복 등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,7 @@
 package hello.core;
 
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -8,9 +10,12 @@ import static org.springframework.context.annotation.ComponentScan.*;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core", // 모든 패키지를 검색하면 오래걸린다. 필요한 패키지만 검색한다.
-        basePackageClasses = AutoAppConfig.class, // AutoAppConfig 자체를 BasePackage로 등록
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes =
                 Configuration.class))
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
