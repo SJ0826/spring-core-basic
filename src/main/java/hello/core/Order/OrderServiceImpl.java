7ce0eeb1922129159c36e9d3710892e4b9bb524f
SJ0826 <ikosdu60@gmail.com>
2024-10-07 22:20:54 +0900

    [Section8] 다양한 의존관계 주입 방법

--- src/main/java/hello/core/Order/OrderServiceImpl.java
+++ src/main/java/hello/core/Order/OrderServiceImpl.java
@@ -10,15 +10,40 @@ import org.springframework.stereotype.Component;
 public class OrderServiceImpl implements OrderService {
 
     // 순수하게 인터페이스에만 의존하고 있음 -> DIP
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+
+    // 수정자
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+    }
 
     @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("discountPolicy = " + discountPolicy);
+        this.discountPolicy = discountPolicy;
+    }
+
+    // @Autowired
+    // 의존 관계 자동 주입
+    // 생성자 호출 시점에 딱 한번만 호출 보장
+    // "불변, 필수" 의존관계에서 사용
+    // 생성자가 하나만 있다면 Autowired 생략 가능
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
 
+    // 일반 메서드 주입
+    // 일반 메서드를 통해 주입받는다.
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
