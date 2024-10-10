ebc729457e495deee650c44646ebbb5f3d168e6f
SJ0826 <ikosdu60@gmail.com>
2024-10-10 22:30:23 +0900

    [section8] @Autowired 필드 명, @Qualifier, @Primary

--- src/main/java/hello/core/Order/OrderServiceImpl.java
+++ src/main/java/hello/core/Order/OrderServiceImpl.java
@@ -4,15 +4,21 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component // 스프링 빈으로 등록
-@RequiredArgsConstructor  // 객체에 final이 붙은 값으로 생성자를 자동으로 생성
+//@RequiredArgsConstructor  // 객체에 final이 붙은 값으로 생성자를 자동으로 생성
 public class OrderServiceImpl implements OrderService {
 
     // 순수하게 인터페이스에만 의존하고 있음 -> DIP
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
 
     @Override
@@ -22,5 +28,5 @@ public class OrderServiceImpl implements OrderService {
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
-    
+
 }
--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,9 +2,12 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary // 여러개의 빈중 우선순위를 가짐
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
