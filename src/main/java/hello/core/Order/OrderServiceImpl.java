06402bf63aff330a4f6337a6ea66291997b7fe7d
SJ0826 <ikosdu60@gmail.com>
2024-10-10 21:54:28 +0900

    [section8] 롬복과 최신 트랜드

--- src/main/java/hello/core/Order/OrderServiceImpl.java
+++ src/main/java/hello/core/Order/OrderServiceImpl.java
@@ -3,26 +3,17 @@ package hello.core.Order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component // 스프링 빈으로 등록
+@RequiredArgsConstructor  // 객체에 final이 붙은 값으로 생성자를 자동으로 생성
 public class OrderServiceImpl implements OrderService {
 
     // 순수하게 인터페이스에만 의존하고 있음 -> DIP
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-
-    // @Autowired
-    // 의존 관계 자동 주입
-    // 생성자 호출 시점에 딱 한번만 호출 보장
-    // "불변, 필수" 의존관계에서 사용
-    // 생성자가 하나만 있다면 Autowired 생략 가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -31,9 +22,5 @@ public class OrderServiceImpl implements OrderService {
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
-
-    // 테스트 용도
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
-    }
+    
 }
